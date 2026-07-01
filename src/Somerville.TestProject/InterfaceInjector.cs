dbe5eea4efcae94e616f052821ad897380043a53
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-06-30 23:17:44 -0500

    feat: port testing code

---
+++ src/Somerville.TestProject/InterfaceInjector.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using Somerville.Primitives.CLR.VM;
+
+namespace Somerville.TestProject;
+
+public static class InterfaceInjector
+{
+    private static AssemblyName SyntheticParentAsmName = new("InterfaceInjection.SyntheticParents");
+    private static AssemblyBuilder? _syntheticParentsAsm;
+
+    private static AssemblyBuilder SyntheticParentsAsm
+    {
+        get
+        {
+            _syntheticParentsAsm ??= AssemblyBuilder.DefineDynamicAssembly(SyntheticParentAsmName, AssemblyBuilderAccess.Run);
+            return _syntheticParentsAsm;
+        }
+    }
+
+    private static ModuleBuilder? _syntheticParentsMod;
+
+    private static ModuleBuilder SyntheticParentsMod
+    {
+        get
+        {
+            _syntheticParentsMod ??= SyntheticParentsAsm.DefineDynamicModule(SyntheticParentAsmName.Name!);
+            return _syntheticParentsMod;
+        }
+    }
+
+    public static string DbgPrintTree(Type child)
+    {
+        string InnerPrintTree(MethodTable handle)
+        {
+            var sb = new StringBuilder();
+            sb.Append($"{handle.DangerousGetHandle():X}");
+            handle = handle.ParentMethodTable;
+
+            while (handle.DangerousGetHandle() != 0)
+            {
+                sb.Append(" -> ");
+                sb.Append($"{handle.DangerousGetHandle():X}");
+                handle = handle.ParentMethodTable;
+            }
+
+            return sb.ToString();
+        }
+
+        var handle = MethodTable.FromType(child);
+        return $"{child.FullName}: {InnerPrintTree(handle)}";
+    }
+
+    public static void InjectInterface(Type targetType, Type injectedType, params ReadOnlySpan<MethodInfo> defs)
+    {
+        if (targetType.FullName is null)
+        {
+            throw new ArgumentException("Type must not be a generic type.", nameof(targetType));
+        }
+
+        var syntheticParentImplBuilder = SyntheticParentsMod.DefineType($"__IMPL__.{targetType.FullName}", TypeAttributes.Public | TypeAttributes.Interface | TypeAttributes.Abstract);
+        syntheticParentImplBuilder.AddInterfaceImplementation(injectedType);
+
+        foreach (var def in defs)
+        {
+            var implBuilder = syntheticParentImplBuilder.DefineMethod($"{injectedType.Name}.{def.Name}", MethodAttributes.Virtual | MethodAttributes.Final, typeof(int), null);
+            var il = implBuilder.GetILGenerator();
+            il.Emit(OpCodes.Ldc_I4, 22);
+            il.Emit(OpCodes.Ret);
+            syntheticParentImplBuilder.DefineMethodOverride(implBuilder, def);
+        }
+
+        var syntheticParentImplType = syntheticParentImplBuilder.CreateType();
+
+        var syntheticParentBuilder = SyntheticParentsMod.DefineType($"__PARENT__.{targetType.FullName}", TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.Abstract, targetType.BaseType);
+        syntheticParentBuilder.AddInterfaceImplementation(syntheticParentImplType);
+        var syntheticParentType = syntheticParentBuilder.CreateType();
+
+        var targetMT = MethodTable.FromType(targetType);
+        var syntheticParentMT = MethodTable.FromType(syntheticParentType);
+        var injectedMT = MethodTable.FromType(injectedType);
+
+        targetMT.ParentMethodTable = syntheticParentMT;
+
+        unsafe
+        {
+            var newSize = targetMT.InterfaceCount + 1;
+            var newInterfaceMapPtr = Marshal.AllocHGlobal(newSize * sizeof(nint));
+            var newInterfaceMap = new Span<MethodTable>((void*)newInterfaceMapPtr, newSize);
+            targetMT.InterfaceMap.CopyTo(newInterfaceMap);
+
+            // Add new interface entry
+            newInterfaceMap[targetMT.InterfaceCount] = injectedMT;
+
+            // Update target's MethodTable
+            targetMT.InterfaceMapPtr = newInterfaceMapPtr;
+            targetMT.InterfaceCount++;
+        }
+    }
+}
---
+++ src/Somerville.TestProject/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Somerville.TestProject;
+
+var sanity = new SanityB();
+Console.WriteLine($"SANITY: {sanity is IBar}");
+
+var baz = new Baz();
+Console.WriteLine(InterfaceInjector.DbgPrintTree(typeof(Baz)));
+Console.WriteLine(InterfaceInjector.DbgPrintTree(typeof(Foo)));
+Console.WriteLine($"Baz::BaseType: {baz.GetType().BaseType?.AssemblyQualifiedName}");
+Console.WriteLine($"Baz::BaseType.BaseType: {baz.GetType().BaseType?.BaseType?.AssemblyQualifiedName}");
+Console.WriteLine($"baz is IBar = {baz is IBar}");
+Console.WriteLine($"baz.HatedNumber() = {baz.HatedNumber()}");
+
+Console.WriteLine();
+
+InterfaceInjector.InjectInterface(typeof(Foo), typeof(IBar), typeof(IBar).GetMethod("FavouriteNumber")!);
+Console.WriteLine(InterfaceInjector.DbgPrintTree(typeof(Baz)));
+Console.WriteLine(InterfaceInjector.DbgPrintTree(typeof(Foo)));
+
+Console.WriteLine($"Baz::BaseType: {baz.GetType().BaseType!.AssemblyQualifiedName}");
+Console.WriteLine($"Baz::BaseType.BaseType: {baz.GetType().BaseType?.BaseType?.AssemblyQualifiedName}");
+Console.WriteLine($"baz is IBar = {baz is IBar}");
+
+// var method = typeof(Foo).GetMethod("IBar.FavouriteNumber", BindingFlags.Instance | BindingFlags.NonPublic);
+// var ifaceMethod = typeof(IBar).GetMethod("FavouriteNumber")!;
+// Console.WriteLine($"decl invocation: {method.Invoke(foo, [])}");
+// Console.WriteLine($"def invocation: {ifaceMethod.Invoke(foo, [])}");
+Console.WriteLine($"baz.HatedNumber() = {baz.HatedNumber()}");
+Console.WriteLine($"((IBar)(object)baz).FavouriteNumber() = {((IBar)(object)baz).FavouriteNumber()}");
+
+// Console.WriteLine();
+
+// Console.WriteLine($"decl fptr: {method.MethodHandle.GetFunctionPointer()}");
+// Console.WriteLine($"def fptr: {ifaceMethod.MethodHandle.GetFunctionPointer()}");
+// Console.WriteLine($"natural fptr: {((Func<int>)((IBar)(object)foo).FavouriteNumber).Method.MethodHandle.GetFunctionPointer()}");
+
+// void WriteArr<T>(T[] arr) {
+//     Console.Write("[ ");
+//     foreach (var elem in arr) {
+//         Console.Write(elem?.ToString() ?? "null");
+//         Console.Write(", ");
+//     }
+//     Console.WriteLine("]");
+// }
+
+public class Foo
+{
+    public int HatedNumber()
+    {
+        return 0xb;
+    }
+}
+
+public class Baz : Foo { }
+
+public interface IBar
+{
+    int FavouriteNumber();
+}
+
+public interface IBarBar : IBar
+{
+    int IBar.FavouriteNumber()
+    {
+        return 44;
+    }
+}
+
+public class SanityA : IBarBar { }
+
+public class SanityB : SanityA { }
